7ce20a450617e83abd4c28f91d1dc92b419116af
Badsha Hossain <badshab454@gmail.com>
2022-12-19 19:22:00 +0600

    m

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -8,32 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       <h2>Hello, React!</h2>
       <p>Badsha Hossain.</p>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Badsha</td>
-            <td>Coder</td>
-          </tr>
-          <tr>
-            <td>Hossain</td>
-            <td>Bouncer</td>
-          </tr>
-          <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-          </tr>
-          <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-          </tr>
-        </tbody>
-      </table>
+      
       </header>
     </div>
   );
